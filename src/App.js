2a4c8ad3309114bdbb4252fe7d2334a800da727a
abir <abirk.kotti@gmail.com>
2020-10-23 23:57:29 +0100

    added menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-function App() {
-  return (
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return(
     <div className="App">
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-      </div>
-    </Navbar>
-  </div>
-  );
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <Menu dishes={this.state.dishes} />
+        </div>
+      </Navbar>
+    </div>
+    )};
 }
 
 export default App;
