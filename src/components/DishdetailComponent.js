b1ae3dafefb7a74bfa8561b2178dde6da660f1ac
abir <abirk.kotti@gmail.com>
2020-10-24 21:02:37 +0100

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,49 +2,55 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
+function RenderDish({ dish }) {
+    return (
+        <Card className="col-12 col-md-5 m-1">
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
 
-class Dishdetail extends Component {
-
-    renderComments(dish) {
-        return (
-                 <Card className="col-12 col-md-5 m-1">
-                    <CardBody>
-                        <CardTitle>Comments</CardTitle>
-                        {
-                            dish.comments.map((item, index) => {
-                                return (
-                                    <ListGroup style={{marginBottom:10}}>
-                                        <ListGroupItem>{item.author}</ListGroupItem>
-                                        <ListGroupItem>{item.comment}</ListGroupItem>
-                                        <ListGroupItem>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(item.date)))}</ListGroupItem>
-                                    </ListGroup>
-
-                                )
-                            })
-                        }
-                    </CardBody>
-                </Card>
-        )
-    }
-    render() {
-        var dish = this.props.dish
-        console.log(dish)
-        return (
-            dish != null ?
-                <div className="container">
-                    <Card className="col-12 col-md-5 m-1">
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                    {this.renderComments(dish)}
-                </div> :
-                <div></div>
-        );
-    }
+    );
 }
+
+function RenderComments({ dish }) {
+    return (
+        <Card className="col-12 col-md-5 m-1">
+            <CardBody>
+                <CardTitle>Comments</CardTitle>
+                {
+                    dish.comments.map((item, index) => {
+                        return (
+                            <ListGroup style={{ marginBottom: 10 }}>
+                                <ListGroupItem>{item.author}</ListGroupItem>
+                                <ListGroupItem>{item.comment}</ListGroupItem>
+                                <ListGroupItem>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(item.date)))}</ListGroupItem>
+                            </ListGroup>
+
+                        )
+                    })
+                }
+            </CardBody>
+        </Card>
+    )
+}
+
+const Dishdetail = (props) => {
+    var dish = props.dish
+    console.log(dish)
+
+    return (
+        dish != null ?
+            <div className="container">
+                <RenderDish dish={dish} />
+                <RenderComments dish={dish} />
+            </div> :
+            <div></div>
+    )
+}
+
 
 export default Dishdetail;
 
