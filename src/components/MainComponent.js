b882e8705720675d78ef1a4cd74421500dfab2c6
abir <abirk.kotti@gmail.com>
2020-10-30 20:40:45 +0100

    change contactForm using redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,24 +6,15 @@ import { DISHES } from '../shared/dishes';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+
 
 class Main extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            promotions: PROMOTIONS,
-            leaders: LEADERS
-        };
-    }
+
 
     onDishSelect(dishId) {
         this.setState({ selectedDish: dishId });
@@ -32,43 +23,52 @@ class Main extends Component {
     render() {
         const DishWithId = ({ match }) => {
             return (
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+                <Dishdetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             );
         };
         const HomePage = () => {
             return (
                 <Home
-                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-        } 
+        }
 
         const AboutPage = () => {
             return (
                 <About leaders={this.state.leaders} />
             )
-
         }
 
         return (
-
             <div>
                 <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' component={AboutPage} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-                    <Route path='/menu/:dishId' component={DishWithId} />
-                    <Redirect to="/home" />
-                </Switch>
+                <div>
+                    <Switch>
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+                        <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                        <Route path='/menu/:dishId' component={DishWithId} />
+                        <Route exact path='/contactus' component={Contact} />
+                        <Redirect to="/home" />
+                    </Switch>
+                </div>
                 <Footer />
             </div>
         );
     }
+
 }
 
-export default Main;
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
+export default withRouter(connect(mapStateToProps)(Main));
